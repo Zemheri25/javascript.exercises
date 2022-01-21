97c9015bc939254530fc8e9cd1d83dafed2bcbbe
Zemheri25 <omerzemheri14@gmail.com>
2022-01-21 18:28:17 +0300

    exercise5 added

---
+++ exercise5.js
@@ -0,0 +1,16 @@
+function keysAndValues (object1) {
+    let keys = Object.keys(object1)
+    let values = Object.values(object1)
+    let array = []
+    array.push(keys, values)
+    return array
+}
+    
+object2 = { a: 1, b: 2, c: 3 }
+object3 = { a: "Apple", b: "Microsoft", c: "Google" }
+object4 = { key1: true, key2: false, key3: undefined }
+
+
+console.log(keysAndValues(object2))
+console.log(keysAndValues(object3))
+console.log(keysAndValues(object4))
