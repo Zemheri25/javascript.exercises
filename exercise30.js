8ba658b77436353f911ce45f4e0f4d6b402e400f
Zemheri25 <omerzemheri14@gmail.com>
2022-02-15 17:56:10 +0300

    exercise 30 added

---
+++ exercise30.js
@@ -0,0 +1,9 @@
+function repetiton(string1, seperator, number1) {
+    let mystr = ""
+    for(let i = 0; i < number1; i++) {
+        mystr += string1 + seperator
+    }
+    return mystr.substring(0, mystr.length-1)
+}
+
+console.log(repetiton("Tina", "!", 3))
