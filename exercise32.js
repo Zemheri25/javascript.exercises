6eb7cb500bb6b5a4e4ddfb108161d63213157bbf
Zemheri25 <omerzemheri14@gmail.com>
2022-02-23 23:08:28 +0300

    find the missing min positive number.İf there is no min return +1 of max.

---
+++ exercise32.js
@@ -0,0 +1,32 @@
+function countingMiss(array1) {
+    let newarr = [];
+    let newarr2 = [];
+    let newarr3 = [];
+    for (let i = Math.min(...array1); i <= Math.max(...array1); i++) {
+        newarr.push(i)
+    }
+    for (let i = 0 ; i < newarr.length; i++) {
+        if(!array1.includes(newarr[i])) {
+            newarr2.push(newarr[i])
+        }
+    }
+
+    if(newarr2.length != 0) {
+    for (let i = 0; i < newarr2.length; i ++ ) {
+        if(newarr2[i] > 0) {
+            newarr3.push(newarr2[i])
+        }    
+    }
+
+        return Math.min(...newarr3)
+    } else {
+        return Math.max(...array1) + 1
+    }
+
+}
+
+console.log(countingMiss([-1,0,2,3 ]));
+
+
+
+
